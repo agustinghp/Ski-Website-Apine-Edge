9efa13e138d59860869011088dc8ff05e282ec7d
Agustin Garcia-Huidobro <agustingarcia-huidobro@Agustins-MacBook-Pro.local>
2025-11-02 18:33:34 -0700

    Fixed Syntax errors

--- Create-Tables.sql
+++ Create-Tables.sql
@@ -1,22 +1,21 @@
-Create Table Products(
-    ProductID INT PRIMARY KEY,
-    id INT,
-    CONSTRAINT user_product FOREIGN KEY(id) REFERENCES Users(id)
+CREATE TABLE Products (
+    ProductID SERIAL PRIMARY KEY,
+    id INT NOT NULL,
+    CONSTRAINT user_product FOREIGN KEY (id) REFERENCES Users(id),
     ProductName VARCHAR(50) NOT NULL,
     ProductDescription VARCHAR(500),
     Brand VARCHAR(50) NOT NULL,
     Model VARCHAR(50) NOT NULL,
-    SkiLength DECIMAL(3,2),
-    SkiWidth DECIMAL(3,2),
-    Price DECIMAL(5,2),
-
+    SkiLength DECIMAL(4,1),
+    SkiWidth DECIMAL(4,1),
+    Price DECIMAL(7,2)
 );
 
-Create Table Services(
-    ServiceID INT PRIMARY KEY,
-    id INT,
-    CONSSTRAINT user_services FOREIGN KEY(id) REFERENCES Users(id)
+CREATE TABLE Services (
+    ServiceID SERIAL PRIMARY KEY,
+    id INT NOT NULL,
+    CONSTRAINT user_services FOREIGN KEY (id) REFERENCES Users(id),
     ServiceName VARCHAR(50) NOT NULL,
     ServiceDescription VARCHAR(500),
-    Price DECIMAL(5,2)
-)
+    Price DECIMAL(7,2)
+);
